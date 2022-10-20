e348c95d23d6395e091a3e2622f077e45f3d688f
anupekshajain <anupeksha.jain@oneadvanced.com>
2022-10-20 17:42:42 +0530

    Merge branch 'main' of https://github.com/anupekshajain/TestProject2.git into main

--- src/test/java/com/mytests/DateFunction.java
+++ src/test/java/com/mytests/DateFunction.java
@@ -9,42 +9,42 @@ import java.util.TimeZone;
 public class DateFunction 
 {
 	
-	public static void main(String[] args)
-	{
-//		Date d = java.util.Calendar.getInstance(java.util.TimeZone.getTimeZone("Europe/Madrid")).getTime();
-//		System.out.println(d.toGMTString());
-//		String da = DDMainFunctions.getDateVal(d);
-//		System.out.println(da);
-		Date now = new Date();	
-		TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"));
-        System.out.println(now);	
-        TimeZone.setDefault( TimeZone.getTimeZone("GMT"));
-        System.out.println(now);
-        TimeZone.setDefault( TimeZone.getTimeZone("UTC"));
-        System.out.println(now);
-		
-////		System.out.println(d.getDay());
-////		System.out.println(d.getMonth());
-////		System.out.println(d.getYear());
-////		System.out.println(d.getDay()+"/"+d.getMonth()+"/"+(d.getYear()+1900));
-////		
-////		Calendar cal = new GregorianCalendar(d.getYear()+1900, d.getMonth(), d.getDay());
-////	//		return (d.getDay()+"/"+d.getMonth()+"/"+(d.getYear()+1900) + " " + d.getHours()+":"+d.getMinutes())	;
-////		String da = (d.getDay()+" "+cal.get(Calendar.MONDAY)+" "+ " " + d.getHours()+":"+d.getMinutes());
+//	public static void main(String[] args)
+//	{
+////		Date d = java.util.Calendar.getInstance(java.util.TimeZone.getTimeZone("Europe/Madrid")).getTime();
+////		System.out.println(d.toGMTString());
+////		String da = DDMainFunctions.getDateVal(d);
 ////		System.out.println(da);
+//		Date now = new Date();	
+//		TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"));
+//        System.out.println(now);	
+//        TimeZone.setDefault( TimeZone.getTimeZone("GMT"));
+//        System.out.println(now);
+//        TimeZone.setDefault( TimeZone.getTimeZone("UTC"));
+//        System.out.println(now);
 //		
+//////		System.out.println(d.getDay());
+//////		System.out.println(d.getMonth());
+//////		System.out.println(d.getYear());
+//////		System.out.println(d.getDay()+"/"+d.getMonth()+"/"+(d.getYear()+1900));
+//////		
+//////		Calendar cal = new GregorianCalendar(d.getYear()+1900, d.getMonth(), d.getDay());
+//////	//		return (d.getDay()+"/"+d.getMonth()+"/"+(d.getYear()+1900) + " " + d.getHours()+":"+d.getMinutes())	;
+//////		String da = (d.getDay()+" "+cal.get(Calendar.MONDAY)+" "+ " " + d.getHours()+":"+d.getMinutes());
+//////		System.out.println(da);
+////		
+////		
+////		Calendar cal=Calendar.getInstance();
+////		SimpleDateFormat month_date = new SimpleDateFormat("MMM");
+////		String month_name = month_date.format(d);
+////		System.out.println("Month :: " + month_name);
+////		String dateVal = DDMainFunctions.getDateVal(now);
+////		System.out.println(dateVal);
 //		
-//		Calendar cal=Calendar.getInstance();
-//		SimpleDateFormat month_date = new SimpleDateFormat("MMM");
-//		String month_name = month_date.format(d);
-//		System.out.println("Month :: " + month_name);
-//		String dateVal = DDMainFunctions.getDateVal(now);
-//		System.out.println(dateVal);
-		
-		Date d = new Date();
-		TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"));
-		System.out.println(DDMainFunctions.getDateVal(d));
-		
-	}
+//		Date d = new Date();
+//		TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"));
+//		System.out.println(DDMainFunctions.getDateVal(d));
+//		
+//	}
 
 }
--- src/test/java/com/mytests/DigDic2.java
+++ src/test/java/com/mytests/DigDic2.java
@@ -16,77 +16,78 @@ import org.testng.annotations.Test;
 
 
 import io.appium.java_client.windows.WindowsDriver;
+import io.appium.java_client.windows.WindowsElement;
 
 public class DigDic2 
 {
-	public static WindowsDriver driver = null;
-//	public static WebDriverWait wait;
-
-	@BeforeSuite
-	public void setUp() 
-	{
-		driver = DDMainFunctions.setUpMethod(driver);
-//		wait = new WebDriverWait(driver, 8);
-	}
-
-	@Ignore
-	@Test
-	public void TestCase1() 
-	{
-		
-		DDMainFunctions.aDDLogIn(driver, "PrathuA", "Password@1");
-		DDMainFunctions.recordDictation(driver, 5);
-		String jobName = "Anu_Winapp 004";
-		DDMainFunctions.sendJobOut(driver, "File note", "Test1 [3 mins]", "Advanced Speech Recognition", jobName);
-		WebElement jobFound = DDMainFunctions.ifJobExists(driver, jobName);
-		System.out.println("Row number : " +jobFound.getAttribute("GridItem.Row"));
-	}
-	
-	@Ignore
-	@Test
-	public void TestCase2()
-	{
-		DDMainFunctions.aDDLogIn(driver, "PrathuA", "Password@1");
-		String jobName = "Anu_Winapp 005";
-//		String 
-		WebElement jobFound = DDMainFunctions.ifJobExists(driver, jobName);
-		if(jobFound!=null)
-		{
-			String rowNum = jobFound.getAttribute("GridItem.Row");
-			Date d = new Date();
-			String jobDate = DDMainFunctions.getDateVal(d);
-			System.out.println(jobDate);
-			DDMainFunctions.verifyJob(driver, rowNum, jobName, jobDate, "Pending");	
-		}
-		}
-	
-	@Ignore
-	@Test
-	public void TestCase3()
-	{
-		DDMainFunctions.aDDLogIn(driver, "authornew", "Password@1");
-		Date d = new Date();
-		String jobDate = DDMainFunctions.getDateVal(d);
-		WebElement jobsTable = driver.findElementByAccessibilityId("JColResizer0");
-		driver.findElement(By.name("<All dates>"));
-	}
-
-	
-	@Test
-	public void TestCase4()
-	{
-		DDMainFunctions.aDDLogIn(driver, "authornew", "Password@1");
-		DDMainFunctions.recordDictation(driver, 3);
-		String jobName = "Anu_Winapp 016";
-		String createdDate = DDMainFunctions.sendJobOut(driver, "File note", "P5 [24 hrs]", "Typing", jobName);
-	//	String jobName = "Enable sett 011";
-	//	String jobName = "Venkat";
-	//	WebElement job = driver.findElementByName(jobName);		
-	//	Date d = new Date();
-	//	String jobDate = DDMainFunctions.getDateVal(d);
-	//	String createdDate = "17 Nov 11:35";
-		DDMainFunctions.verifyJobWithCreatedDate(driver, createdDate, jobName, "Pending");
-	}
+//	public static WindowsDriver<WebElement> driver = null;
+////	public static WebDriverWait wait;
+//
+//	@BeforeSuite
+//	public void setUp() 
+//	{
+//		driver = DDMainFunctions.setUpMethod(driver);
+////		wait = new WebDriverWait(driver, 8);
+//	}
+//
+//	@Ignore
+//	@Test
+//	public void TestCase1() 
+//	{
+//		
+//		DDMainFunctions.aDDLogIn(driver, "PrathuA", "Password@1");
+//		DDMainFunctions.recordDictation(driver, 5);
+//		String jobName = "Anu_Winapp 004";
+//		DDMainFunctions.sendJobOut(driver, "File note", "Test1 [3 mins]", "Advanced Speech Recognition", jobName);
+//		WebElement jobFound = DDMainFunctions.ifJobExists(driver, jobName);
+//		System.out.println("Row number : " +jobFound.getAttribute("GridItem.Row"));
+//	}
+//	
+//	@Ignore
+//	@Test
+//	public void TestCase2()
+//	{
+//		DDMainFunctions.aDDLogIn(driver, "PrathuA", "Password@1");
+//		String jobName = "Anu_Winapp 005";
+////		String 
+//		WebElement jobFound = DDMainFunctions.ifJobExists(driver, jobName);
+//		if(jobFound!=null)
+//		{
+//			String rowNum = jobFound.getAttribute("GridItem.Row");
+//			Date d = new Date();
+//			String jobDate = DDMainFunctions.getDateVal(d);
+//			System.out.println(jobDate);
+//			DDMainFunctions.verifyJob(driver, rowNum, jobName, jobDate, "Pending");	
+//		}
+//		}
+//	
+//	@Ignore
+//	@Test
+//	public void TestCase3()
+//	{
+//		DDMainFunctions.aDDLogIn(driver, "authornew", "Password@1");
+//		Date d = new Date();
+//		String jobDate = DDMainFunctions.getDateVal(d);
+//		WebElement jobsTable = driver.findElementByAccessibilityId("JColResizer0");
+//		driver.findElement(By.name("<All dates>"));
+//	}
+//
+//	
+//	@Test
+//	public void TestCase4()
+//	{
+//		DDMainFunctions.aDDLogIn(driver, "authornew", "Password@1");
+//		DDMainFunctions.recordDictation(driver, 3);
+//		String jobName = "Anu_Winapp 016";
+//		String createdDate = DDMainFunctions.sendJobOut(driver, "File note", "P5 [24 hrs]", "Typing", jobName);
+//	//	String jobName = "Enable sett 011";
+//	//	String jobName = "Venkat";
+//	//	WebElement job = driver.findElementByName(jobName);		
+//	//	Date d = new Date();
+//	//	String jobDate = DDMainFunctions.getDateVal(d);
+//	//	String createdDate = "17 Nov 11:35";
+//		DDMainFunctions.verifyJobWithCreatedDate(driver, createdDate, jobName, "Pending");
+//	}
 }
 //	@AfterSuite
 //	public void tearDown() 
